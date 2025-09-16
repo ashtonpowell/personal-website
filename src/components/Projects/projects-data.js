d91c8bc43268dfa12f0815d851f3e8b9065e609f
ashtonpowell <ashton.s.powell@gmail.com>
2025-09-16 10:06:32 -0700

    added new projects to project section

--- src/components/Projects/projects-data.js
+++ src/components/Projects/projects-data.js
@@ -9,11 +9,11 @@ const projectData = [
     id: 1,
     type: "project",
     discipline: "compSci",
-    title: "Personal Website",
+    title: "Portfolio Website",
     description:
-      "Link to the public GitHub repository for my personal website.",
+      "The website you're on now! Used to share my experience, skills, and works.",
     image: "assets/project-img/personal-website.png",
-    imgAlt: "Personal Website",
+    imgAlt: "Portfolio Website",
     date: "2025",
     link: "https://github.com/ashtonpowell/personal-website",
   },
@@ -23,7 +23,7 @@ const projectData = [
     discipline: "compSci",
     title: "Secure Password Manager",
     description:
-      "Link to the public GitHub repository for the Secure Password Manager project.",
+      "A browser application that allows users to safely store all their login credentials for different websites/apps in one place.",
     image: "assets/project-img/secure-password.png",
     imgAlt: "Secure Password Manager",
     date: "2025",
@@ -35,7 +35,7 @@ const projectData = [
     discipline: "compSci",
     title: "Drug Shortages",
     description:
-      "Link to the public GitHub repository for the Drug Shortages project.",
+      "Link to the public GitHub repository for the Drug Shortages project. (Work in Progress)",
     image: "assets/project-img/openfda.png",
     imgAlt: "Drug Shortages Project",
     date: "2025",
@@ -43,6 +43,30 @@ const projectData = [
   },
   {
     id: 4,
+    type: "project",
+    discipline: "compSci",
+    title: "RDT Python Simulation",
+    description:
+      "A Python-based simulation of a Reliable Data Transfer (RDT) protocol over an unreliable network channel.",
+    image: "assets/project-img/rdt.png",
+    imgAlt: "RDT Python Simulation",
+    date: "2025",
+    link: "https://github.com/ashtonpowell/rdt-python-simulation",
+  },
+  {
+    id: 5,
+    type: "project",
+    discipline: "compSci",
+    title: "ChessVar",
+    description:
+      "A Python implementation of a chess variant game with custom rules and pieces.",
+    image: "assets/project-img/chess-var.png",
+    imgAlt: "ChessVar",
+    date: "2025",
+    link: "https://github.com/ashtonpowell/chess-variant",
+  },
+  {
+    id: 6,
     type: "project",
     discipline: "chemistry",
     title: "Undergraduate Research Thesis Project",
