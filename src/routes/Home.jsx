6da4fb46b3710912fa1cbd3b89a80261bb916c99
ashtonpowell <ashton.s.powell@gmail.com>
2025-09-20 17:14:50 -0700

    added routing

---
+++ src/routes/Home.jsx
@@ -0,0 +1,56 @@
+import Header from "../components/Header/Header";
+import Intro from "../components/Intro/Intro";
+import AboutMe from "../components/AboutMe/AboutMe";
+import MyWorks from "../components/MyWorks/MyWorks";
+import ContactMe from "../components/ContactMe/ContactMe";
+import Footer from "../components/Footer/Footer";
+import { motion, AnimatePresence } from "framer-motion";
+
+export default function Home() {
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key="app-root"
+        initial={{ opacity: 0, y: 0 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 2, ease: "easeInOut", delay: 0.15 }}
+      >
+        <Header />
+        <motion.div
+          initial={{ opacity: 0, y: 0 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false, amount: 0.2 }}
+          transition={{ duration: 0.7, delay: 0.15 }}
+        >
+          <Intro />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 0 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false, amount: 0.2 }}
+          transition={{ duration: 0.7, delay: 0.15 }}
+        >
+          <AboutMe />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 0 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false, amount: 0.2 }}
+          transition={{ duration: 0.7, delay: 0.15 }}
+        >
+          <MyWorks />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 0 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: false, amount: 0.2 }}
+          transition={{ duration: 0.7, delay: 0.15 }}
+        >
+          <ContactMe />
+        </motion.div>
+        <Footer />
+      </motion.div>
+    </AnimatePresence>
+  );
+}
---
+++ src/routes/NotFound.jsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-2">
+      <h1 className="text-6xl font-bold sm:text-7xl md:text-8xl lg:text-9xl">
+        404
+      </h1>
+      <p className="text-base text-zinc-300/90 sm:text-lg md:text-xl lg:text-3xl">
+        You might be lost my friend...
+      </p>
+    </div>
+  );
+}
